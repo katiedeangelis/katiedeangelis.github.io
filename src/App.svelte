fc117609eab66c31d9ac9f28e99c61fd120c101e
kdeangelis1 <k.deangelis1@snhu.edu>
2021-01-14 09:40:02 -0500

    SCSS integration

--- src/App.svelte
+++ src/App.svelte
@@ -7,24 +7,19 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style type="text/scss">
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    $color: red;
+	
+    h1 {
+    color: $color;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    div {
+        background: green;
+
+        > p {
+            color: #fff;
+        }
+    }
 </style>
